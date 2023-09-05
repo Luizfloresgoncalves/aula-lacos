b9e4f36f8abb7c3f543e8105a76f97fd4e86c32e
Luiz <luizfloresgoncalvesneto@gmail.com>
2023-09-04 21:55:49 -0300

    criado aula-lacos

---
+++ index.js
@@ -0,0 +1,10 @@
+//exercicio de interpretacao de codigo
+//1. o codigo vai somando cada indice em que passa ate o indice 4 ,resultando 10
+
+// 2. 
+//a. ele imprime todos os numeros da lista que são maiores que 18 
+//b. não é possivel
+
+//3. vai aparecer *,**,***,**** 
+
+
